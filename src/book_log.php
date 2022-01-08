fa7f764ea37eb1e6ad685aba6471ca68612de072
takicoYuki <takita.cons@gmail.com>
2022-01-08 18:28:36 +0900

    修正

--- src/book_log.php
+++ src/book_log.php
@@ -1,12 +1,12 @@
 <?php
 
-echo '1. 読書ログを登録'.PHP_EOL;
-echo '2. 読書ログを表示' . PHP_EOL;
-echo '9. アプリケーションを終了' . PHP_EOL;
-echo '番号を選択してください (1,2,9) :';
-
-$num = trim(fgets(STDIN));
 while(true){
+    echo '1. 読書ログを登録' . PHP_EOL;
+    echo '2. 読書ログを表示' . PHP_EOL;
+    echo '9. アプリケーションを終了' . PHP_EOL;
+    echo '番号を選択してください (1,2,9) :';
+    $num = trim(fgets(STDIN));
+
     if($num === '9'){
         break;
     }else{
@@ -24,7 +24,7 @@ while(true){
             echo '感想:';
             $thoughts = trim(fgets(STDIN));
             echo '登録が完了しました';
-
+            continue;
         }elseif($num === '2'){
             //TODO 読書ログを表示
             $title = '';
@@ -39,6 +39,7 @@ while(true){
             echo '読書状況:' . $readStatus . PHP_EOL;
             echo '評価:' . $evaluation . PHP_EOL;
             echo '感想:' . $thoughts . PHP_EOL;
+            continue
         }elseif($num === '9'){
             //アプリケーションを終了
         }
