94a7280a1106c85336d3b2a0bb3208e87ac5b9e8
takicoYuki <takita.cons@gmail.com>
2022-01-08 18:40:07 +0900

    test

--- src/book_log.php
+++ src/book_log.php
@@ -7,42 +7,39 @@ while(true){
     echo '番号を選択してください (1,2,9) :';
     $num = trim(fgets(STDIN));
 
-    if($num === '9'){
+    if ($num === '1'){
+        //TODO 読書ログを登録
+        echo '読書ログを登録してください' . PHP_EOL;
+        echo '書籍名:';
+        $title = trim(fgets(STDIN));
+        echo '著者名:';
+        $AuthorName = trim(fgets(STDIN));
+        echo '読書状況:';
+        $readStatus = trim(fgets(STDIN));
+        echo '評価:';
+        $evaluation = trim(fgets(STDIN));
+        echo '感想:';
+        $thoughts = trim(fgets(STDIN));
+        echo '登録が完了しました';
+        continue;
+    }elseif($num === '2'){
+        //TODO 読書ログを表示
+        $title = '';
+        $AuthorName = '';
+        $readStatus = '';
+        $evaluation = '';
+        $thoughts = '';
+        //TODO 後で削除する
+        echo '登録されている読書ログを表示します' . PHP_EOL;
+        echo '書籍名:' . $title . PHP_EOL;
+        echo '著者名:' . $AuthorName . PHP_EOL;
+        echo '読書状況:' . $readStatus . PHP_EOL;
+        echo '評価:' . $evaluation . PHP_EOL;
+        echo '感想:' . $thoughts . PHP_EOL;
+        continue;
+    }elseif($num === '9'){
+        //アプリケーションを終了
         break;
-    }else{
-        if ($num === '1'){
-            //TODO 読書ログを登録
-            echo '読書ログを登録してください' . PHP_EOL;
-            echo '書籍名:';
-            $title = trim(fgets(STDIN));
-            echo '著者名:';
-            $AuthorName = trim(fgets(STDIN));
-            echo '読書状況:';
-            $readStatus = trim(fgets(STDIN));
-            echo '評価:';
-            $evaluation = trim(fgets(STDIN));
-            echo '感想:';
-            $thoughts = trim(fgets(STDIN));
-            echo '登録が完了しました';
-            continue;
-        }elseif($num === '2'){
-            //TODO 読書ログを表示
-            $title = '';
-            $AuthorName = '';
-            $readStatus = '';
-            $evaluation = '';
-            $thoughts = '';
-            //TODO 後で削除する
-            echo '登録されている読書ログを表示します' . PHP_EOL;
-            echo '書籍名:' . $title . PHP_EOL;
-            echo '著者名:' . $AuthorName . PHP_EOL;
-            echo '読書状況:' . $readStatus . PHP_EOL;
-            echo '評価:' . $evaluation . PHP_EOL;
-            echo '感想:' . $thoughts . PHP_EOL;
-            continue;
-        }elseif($num === '9'){
-            //アプリケーションを終了
-        }
     }
 }
 
---
+++ src/tmp/array.php
@@ -0,0 +1,9 @@
+<?php
+    $comics = ['火の鳥','ガラスの仮面'];
+    var_export($comics);
+
+    $comics[] = 'BANANA FISH';
+    var_dump($comics);
+
+    echo $comics[2] .PHP_EOL;
+?>
