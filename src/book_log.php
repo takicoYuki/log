70f020c958decdafc5317e2014e42798cf9d85a3
takicoYuki <takita.cons@gmail.com>
2022-01-08 11:21:49 +0900

    項目追加

--- src/book_log.php
+++ src/book_log.php
@@ -3,9 +3,24 @@
 echo '読書ログを登録してください' .PHP_EOL;
 echo '書籍名:';
 $title = trim(fgets(STDIN));
+echo '著者名:';
+$AuthorName = trim(fgets(STDIN));
+echo '読書状況:';
+$readStatus = trim(fgets(STDIN));
+echo '評価:';
+$evaluation = trim(fgets(STDIN));
+echo '感想:';
+$thoughts = trim(fgets(STDIN));
+echo '登録が完了しました';
+
+
 echo '登録が完了しました'.PHP_EOL .PHP_EOL;
 echo '読書ログを表示します' .PHP_EOL;
 echo '書籍名:'. $title . PHP_EOL;
+echo '著者名:' . $AuthorName . PHP_EOL;
+echo '読書状況:' . $readStatus . PHP_EOL;
+echo '評価:' . $evaluation . PHP_EOL;
+echo '感想:' . $thoughts . PHP_EOL;
 
 // echo '書籍名:銀河鉄道の夜' . PHP_EOL;
 // echo '著者名:宮沢賢治' . PHP_EOL;
