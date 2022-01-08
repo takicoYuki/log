ed8bb6d03c7a323a84797bf6258db4b5fef4ed9b
takicoYuki <takita.cons@gmail.com>
2022-01-08 18:14:47 +0900

    test

--- src/book_log.php
+++ src/book_log.php
@@ -1,31 +1,41 @@
 <?php
 
-echo '読書ログを登録してください' .PHP_EOL;
-echo '書籍名:';
-$title = trim(fgets(STDIN));
-echo '著者名:';
-$AuthorName = trim(fgets(STDIN));
-echo '読書状況:';
-$readStatus = trim(fgets(STDIN));
-echo '評価:';
-$evaluation = trim(fgets(STDIN));
-echo '感想:';
-$thoughts = trim(fgets(STDIN));
-echo '登録が完了しました';
+echo '1. 読書ログを登録'.PHP_EOL;
+echo '2. 読書ログを表示' . PHP_EOL;
+echo '9. アプリケーションを終了' . PHP_EOL;
+echo '番号を選択してください (1,2,9) :';
 
+$num = trim(fgets(STDIN));
 
-echo '登録が完了しました'.PHP_EOL .PHP_EOL;
-echo '読書ログを表示します' .PHP_EOL;
-echo '書籍名:'. $title . PHP_EOL;
-echo '著者名:' . $AuthorName . PHP_EOL;
-echo '読書状況:' . $readStatus . PHP_EOL;
-echo '評価:' . $evaluation . PHP_EOL;
-echo '感想:' . $thoughts . PHP_EOL;
+if ($num === '1'){
+    //TODO 読書ログを登録
+    echo '読書ログを登録してください' . PHP_EOL;
+    echo '書籍名:';
+    $title = trim(fgets(STDIN));
+    echo '著者名:';
+    $AuthorName = trim(fgets(STDIN));
+    echo '読書状況:';
+    $readStatus = trim(fgets(STDIN));
+    echo '評価:';
+    $evaluation = trim(fgets(STDIN));
+    echo '感想:';
+    $thoughts = trim(fgets(STDIN));
+    echo '登録が完了しました';
 
-// echo '書籍名:銀河鉄道の夜' . PHP_EOL;
-// echo '著者名:宮沢賢治' . PHP_EOL;
-// echo '読書状況:読了' . PHP_EOL;
-// echo '評価:5' . PHP_EOL;
-// echo '感想:本当の幸せとは何だろうかと考えさせられる作品だった' . PHP_EOL;
+}elseif($num === '2'){
+    //TODO 読書ログを表示
+    //TODO 後で削除する
+    echo '登録が完了しました' . PHP_EOL . PHP_EOL;
+    echo '読書ログを表示します' . PHP_EOL;
+    echo '書籍名:' . $title . PHP_EOL;
+    echo '著者名:' . $AuthorName . PHP_EOL;
+    echo '読書状況:' . $readStatus . PHP_EOL;
+    echo '評価:' . $evaluation . PHP_EOL;
+    echo '感想:' . $thoughts . PHP_EOL;
+}elseif($num === '9'){
+    //アプリケーションを終了
+
+}
+
 
 ?>
