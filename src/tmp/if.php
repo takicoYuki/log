d3f04a768cffb4b90cddddcf5b6569b872b96d9d
takicoYuki <takita.cons@gmail.com>
2022-01-08 17:35:26 +0900

    test

--- src/tmp/if.php
+++ src/tmp/if.php
@@ -3,9 +3,9 @@ var_dump(1<2);
 var_dump(1>2);
 $a = 1;
 if ($a < 2){
-    echo "a:$aは2より小さい" . PHP_EOL;
+    echo 'a:'. "$aは2より小さい" . PHP_EOL;
 } else {
-    echo "a:$aは2より大きい" . PHP_EOL;
+    echo 'a' . "$aは2より大きい" . PHP_EOL;
 }
 
 ?>
