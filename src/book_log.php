5d6bffe8e70e660a58d4ddf82b1d21667c5ba62a
takicoYuki <takita.cons@gmail.com>
2022-01-08 10:46:19 +0900

    取得表示

---
+++ src/book_log.php
@@ -0,0 +1,16 @@
+<?php
+
+echo '読書ログを登録してください' .PHP_EOL;
+echo '書籍名:';
+$title = fgets(STDIN);
+echo '登録が完了しました'.PHP_EOL .PHP_EOL;
+echo '読書ログを表示します' .PHP_EOL;
+echo $title . PHP_EOL;
+
+// echo '書籍名:銀河鉄道の夜' . PHP_EOL;
+// echo '著者名:宮沢賢治' . PHP_EOL;
+// echo '読書状況:読了' . PHP_EOL;
+// echo '評価:5' . PHP_EOL;
+// echo '感想:本当の幸せとは何だろうかと考えさせられる作品だった' . PHP_EOL;
+
+?>
