99a1ff88d9c75d04235645566c9866c5d0dbea81
takicoYuki <takita.cons@gmail.com>
2022-01-15 23:24:11 +0900

    connectTest

---
+++ src/tmp/mysqli.php
@@ -0,0 +1,12 @@
+<?php
+    $link = mysqli_connect('db','book_log','pass','book_log');
+
+    echo 'データベースに接続できました'.PHP_EOL;
+    if(!$link){
+        echo 'データベースに接続できませんでした'.PHP_EOL;
+        echo 'Error'.mysqli_connect_errno().PHP_EOL;
+    }
+    echo 'データベースに接続できました' . PHP_EOL;
+    mysqli_close($link);
+    echo 'データベースとの接続を切断しました' . PHP_EOL;
+?>
